ce2ee0ac3aacf3f2aab253a2ab74a3f1ca2ba32b
Sergey Medvedev <s.medvedev-vip@yandex.ru>
2026-01-20 15:31:51 +0300

    подготовка к 4 уроку

--- script.js
+++ script.js
@@ -1,37 +1,18 @@
 "use strict";
 
-let rollback = 20;
-let adaptive;
-
-alert("Hello, JS!");
-console.log("Here we go!");
-
 /* lesson03 */
 
 let title = prompt("Как называется ваш проект?");
-console.log(title);
-
 let screens = prompt("Какие типы экранов нужно разработать?");
-console.log(screens);
-
 let screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
-console.log(screenPrice);
-
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
-
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
-
-console.log("услуга " + service1 + " будет стоить " + servicePrice1);
-
-console.log("услуга " + service2 + " будет стоить " + servicePrice2);
-
+let rollback = 20;
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
 let servicePercentPrice = Math.round((fullPrice * (100 - rollback)) / 100);
-
-console.log(servicePercentPrice);
 
 if (fullPrice >= 30000) {
   console.log("Даём скидку 10%");
@@ -42,5 +23,9 @@ if (fullPrice >= 30000) {
 } else if (fullPrice <= 0) {
   console.log("Что-то пошло не так");
 }
+
+console.log("услуга " + service1 + " будет стоить " + servicePrice1);
+console.log("услуга " + service2 + " будет стоить " + servicePrice2);
+console.log(servicePercentPrice);
 
 /* end lesson03 */
